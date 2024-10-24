e4f5ccadda5533b5caeb312025f52d22b24089b0
Ali Hassan <165924119+alihassanml@users.noreply.github.com>
2024-10-24 21:39:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 from ultralytics import YOLO
+import cv2
+class_names = ['0', '1', '2', '3', 'Raising', 'Rasing', 'qiyam', 'ruku', 'sujud'] 
 
-# Load the model
 model = YOLO('best.onnx')
 
-# Run inference
-results = model('namaz.jpg')
+results = model('5.jpg')
 
 result = results[0]
+image = cv2.imread('5.jpg')
 
-# Show results
-result.show()  # Display the results
-
-# Save results
-result.save('output.jpg')
-
-
-
+for box in result.boxes:
+    class_id = int(box.cls.item())  # Convert tensor to int
+    class_name = class_names[class_id]  # Map class ID to class name
+    confidence = box.conf.item()  # Convert tensor to float
     
+    # Get coordinates as a numpy array and convert to list
+    coordinates = box.xyxy[0].cpu().numpy()  # Get the first (and only) box and convert to numpy
+    x1, y1, x2, y2 = map(int, coordinates)  # Convert to integers
 
-# cap = cv2.VideoCapture(0)
-# while True:
-#     __,frame = cap.read()
-    
-#     result = model(frame)[0]
-#     print(result)
-#     cv2.imshow('image',frame)
-    
-#     if cv2.waitKey(1) ==27:
-#         break
-# cv2.destroyAllWindows()
-# cap.release()
+    # Draw bounding box
+    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)  # Draw rectangle
+    font_scale =2  # Increase this value for larger text
+    cv2.putText(image, f"{class_name} {confidence:.2f}", 
+                (x1, y1 - 10), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                font_scale, (124, 252, 0), 
+                2)   # Put text
+
+# Save the output image
+cv2.imwrite('output6.jpeg', image)
+cv2.destroyAllWindows() 
